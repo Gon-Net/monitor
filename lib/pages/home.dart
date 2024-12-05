0b8c5d1b31577643847bbd090d1753f4e1632c23
jcori <gonzalocori@gmail.com>
2024-12-05 11:11:04 -0400

    Creation app monitor climas

---
+++ lib/pages/home.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Monitor Climas'),
+      ),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: LatLng(-17.430397, -63.693940), // Center the map over London
+          initialZoom: 5.6,
+        ),
+        children: [
+          TileLayer( // Display map tiles from any source
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OSMF's Tile Server
+            userAgentPackageName: 'com.example.app',
+            // And many more recommended properties!
+          ),
+          RichAttributionWidget( // Include a stylish prebuilt attribution widget that meets all requirments
+            attributions: [
+              TextSourceAttribution(
+                'OpenStreetMap contributors',
+                onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')), // (external)
+              ),
+              // Also add images...
+            ],
+          ),
+        ],
+      )
+    );
+  }
+}
